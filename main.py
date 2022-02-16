ea2abdb64a2287e013e4693cfbfe83ab4689355f
Amitai Yacobi <amitai@Amitais-MacBook-Pro.local>
2022-02-16 19:25:06 +0200

    some minor changes

--- main.py
+++ main.py
@@ -33,11 +33,11 @@ def wrap_env(env):
 env = gym.make("LunarLanderContinuous-v2")
 env = wrap_env(env)
 
-#-----discritization-----#
+#--------------------------------discritization------------------------------------#
 main_engine_values = [0, 0.5, 1]
 sec_engine_values = [-1, -0.75, 0, 0.75, 1]
 discrete_actions = [(x, y) for x in main_engine_values for y in sec_engine_values]
-#------------------------#
+#----------------------------------------------------------------------------------#
 
 state_size = env.observation_space.shape[0]
 action_size = len(discrete_actions)
